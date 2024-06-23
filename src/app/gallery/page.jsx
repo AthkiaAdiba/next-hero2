acc4d7e34b872cbb2776c2c9516cb0c2b99c284c
thkiaAdiba <athkiaadiba@gmail.com>
2024-06-23 12:27:57 +0600

    added

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -2,23 +2,23 @@ import Image from 'next/image';
 import React from 'react';
 
 
-const getTime = async () => {
-    const res = await fetch("http://localhost:3000/time", { cache: 'no-store' });
-    const data = await res.json()
-    console.log(data);
-    return data.currentTime;
-};
+// const getTime = async () => {
+//     const res = await fetch("http://localhost:3000/time", { cache: 'no-store' });
+//     const data = await res.json()
+//     console.log(data);
+//     return data.currentTime;
+// };
 
 
 
 const page = async () => {
 
-    const currentTime = await getTime();
+    // const currentTime = await getTime();
 
     return (
         <div className='min-h-screen px-12 py-24'>
             <h6 className='text-3xl'>Gallery Page</h6>
-            <h3 className='text-3xl text-red-400 mt-12 mb-12'>Time: {currentTime}</h3>
+            {/* <h3 className='text-3xl text-red-400 mt-12 mb-12'>Time: {currentTime}</h3> */}
             <div>
                 {
                     [1, 2, 3, 4, 5]?.map((img) => (
